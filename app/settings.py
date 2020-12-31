7949b686ff6c1d51d6fc4c4c578e6b93fe769c56
erich-e <erich.elendt@gmail.com>
2020-12-31 23:33:25 +0000

    dockerfile try

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "_zg_^_*7hx^*^e&*07*t(af-1+cw=o)r=zi&ir&!dv1c0p^pz1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 STATIC_URL = "/static/"
